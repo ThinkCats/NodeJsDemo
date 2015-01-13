57ae7613be27ae4bfade3f3dcfe28fc7fdee1489
ThinkCats <624432528@qq.com>
2015-01-13 20:22:35 +0800

    update cheerio.js

--- cheerio/cheerio.js
+++ cheerio/cheerio.js
@@ -1,33 +1,46 @@
 var cheerio= require('cheerio');
 var request=require('request');
+var fs=require('fs');
+var buffer=require('buffer');
+
 var url='http://www.cnblogs.com/juepei/';
 var result=[];
-
-function getDatas(){
-request(url,function(error,res,body){
-	if(!error && res.statusCode == 200){
-		//console.log(body);
-		var $=cheerio.load(body);
-		$('div').filter(function(i,e){
-			if($(this).attr('class')==='postTitle'){
-				console.log($(this).text());
-				result[i]=$(this).text();
-			}
-		});
-	}
-	else{
-		console.log(error);
-	}
-});
+var fs_option={
+	encoding:'utf8'
 }
 
+function getDatas(){
+	request(url,function(error,res,body){
+		if(!error && res.statusCode == 200){
+			//console.log(body);
+			var $=cheerio.load(body);
+			var j=0;
+			$('div').filter(function(i,e){
+				if($(this).attr('class')==='postTitle'){
+					j++;
+					//console.log($(this).text()+j);
+					result[j]=$(this).text();
+					buf=new Buffer(256);
+					buf.write($(this).text(),'utf8');
+					fs.writeFile('/home/wang/data.txt',buf,fs_option,function(err){
+						if(err){
+							console.log('write data err');
+						}
+					});
+				}
+			});
+		}
+		else{
+			console.log(error);
+		}
+	});
+}
 
 function print(arry){
+	console.log(arry);
 	for(var i=0;i<arry.size;i++){
 		console.log(arry[i]);
 	}
 }
 
 getDatas();
-console.log('--------------------------- result -----------------------------');
-print(result);
